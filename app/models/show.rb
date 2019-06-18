25bfbf6a18172f2a534db689b46e6307d2260fec
Brian Salemi <briandsalemi@gmail.com>
2019-06-18 16:35:54 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,4 +4,7 @@ class Show < ActiveRecord::Base
     Show.maximum(:rating)
   end
 
+  def self.most_popular_show
+    Show.find_by_sql("SELECT * FROM shows ORDER BY rating DESC LIMIT 1")
+  end
 end
