00823465bc5652db3fdeb164edcbec5d2ce34ea0
Brian Salemi <briandsalemi@gmail.com>
2019-06-18 16:37:07 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -7,4 +7,8 @@ class Show < ActiveRecord::Base
   def self.most_popular_show
     Show.find_by_sql("SELECT * FROM shows ORDER BY rating DESC LIMIT 1")[0]
   end
+
+  def self.lowest_rating
+    Show.minimum(:rating)
+  end
 end
